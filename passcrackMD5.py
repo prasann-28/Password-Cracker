976375935aa5288b3f93098dbd64087f99ed15ef
Prasann Shimpi <prasann.shimpi@icloud.com>
2020-09-25 23:49:56 +0530

    cracks md5 hashes from wordlist

---
+++ passcrackMD5.py
@@ -0,0 +1,26 @@
+import  hashlib
+
+flag = 0
+counter = 0
+
+pash_hash = input("Enter md5 hash: ")
+
+wordlist = input("File name: ")
+try:
+     pass_file = open (wordlist, "r")
+except:
+    print("No file found")
+    quit()
+for word in pass_file:
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+    counter += 1
+
+    if digest == pash_hash:
+        print("Password Found:")
+        print(word)
+        flag =1
+        break
+
+if flag ==0:
+    print("password/phrase not found")
